db7a0855a43221a5cdd4aad91f8fb14dce2948d0
Waruna7 <kavinduhiroshan17@gmail.com>
2025-05-27 21:10:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,4 +12,7 @@ module.exports = {
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Waruna7/bot-help2/refs/heads/main/ChatGPT%20Image%20May%2016%2C%202025%2C%2011_52_58%20AM.png",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 W_A_R_U_9_9_9🥶",
   MODE: process.env.MODE || "public",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyA9Tpn31BujI0-GUaMgqUmJqBK9XWBEYmA",
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|bfc07870633518de653fc608e775a88f39037522",
+  TMDB_API_KEY: process.env.TMDB_API_KEY || "2619480e652a7d92e47cef44c27e96b5"
 };
